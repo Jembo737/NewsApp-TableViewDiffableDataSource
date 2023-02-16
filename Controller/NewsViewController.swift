27929818a0bc9f6492690e2622ff2e3f9b724e0d
Jembo737 <74143229+Jembo737@users.noreply.github.com>
2023-02-16 22:45:13 +0400

    Implemented Searchbar

--- Controller/NewsViewController.swift
+++ Controller/NewsViewController.swift
@@ -12,15 +12,23 @@ class NewsViewController: UIViewController {
     
     private var tableView: UITableView!
     private var articles = [Article]()
+    private var filteredArticles = [Article]()
     private var dataSource: UITableViewDiffableDataSource<Int, NewsTableViewCellViewModel>!
+    private let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupSearchBar()
         title = "News"
         view.backgroundColor = .systemBackground
         
         fetchTopStories()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     private func setupTableView() {
@@ -37,13 +45,30 @@ class NewsViewController: UIViewController {
             return cell
         })
     }
+    
+
 }
 
 extension NewsViewController {
+    private func fetchTopStories() {
+        DataCaller.shared.getTopStories { [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.articles = articles
+                self?.filteredArticles = articles
+                DispatchQueue.main.async {
+                    self?.applySnapshot()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     private func applySnapshot() {
         var snapshot = NSDiffableDataSourceSnapshot<Int, NewsTableViewCellViewModel>()
         snapshot.appendSections([0])
-        snapshot.appendItems(articles.map({
+        snapshot.appendItems(filteredArticles.map({
             NewsTableViewCellViewModel(
                 title: $0.title,
                 author: $0.author ?? "No Author",
@@ -53,25 +78,18 @@ extension NewsViewController {
         dataSource.apply(snapshot, animatingDifferences: true)
     }
     
-    private func fetchTopStories() {
-        DataCaller.shared.getTopStories { [weak self] result in
-            switch result {
-            case .success(let articles):
-                self?.articles = articles
-                DispatchQueue.main.async {
-                    self?.applySnapshot()
-                }
-            case .failure(let error):
-                print(error)
-            }
-        }
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
     }
+    
 }
 
 extension NewsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let article = articles[indexPath.row]
+        let article = filteredArticles[indexPath.row]
         
         guard let url = URL(string: article.url ?? "") else {
             return
@@ -85,3 +103,14 @@ extension NewsViewController: UITableViewDelegate {
     }
 }
 
+extension NewsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredArticles = articles.filter({ article in
+            if searchText.isEmpty {
+                return true
+            }
+            return article.title.lowercased().contains(searchText.lowercased())
+        })
+        applySnapshot()
+    }
+}
